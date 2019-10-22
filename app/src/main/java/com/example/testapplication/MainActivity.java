93a6be93d897a3e92479d23be9f6c5a534eb0272
remisaado <remi_saado@hotmail.com>
2019-10-22 11:40:18 +0200

    Adds functionality for deleting items in list with left-swipe.

--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.testapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -28,22 +29,15 @@ public class MainActivity extends AppCompatActivity implements BottomSheetDialog
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        for (int i = 0; i < 15; i++)
-        {
-            list.add("Item " + Integer.toString(i + 1));
-        }
-
         fab = findViewById(R.id.floatingActionButton);
         recyclerView = findViewById(R.id.recyclerView);
-        layoutManager = new LinearLayoutManager(this);
 
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
-        dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.divider_line, null));
-        recyclerView.addItemDecoration(dividerItemDecoration);
+        initRecyclerView();
 
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerViewAdapter = new RecyclerViewAdapter(list);
-        recyclerView.setAdapter(recyclerViewAdapter);
+        for (int i = 1; i < 16; i++)
+        {
+            list.add("Item " + i);
+        }
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,10 +49,36 @@ public class MainActivity extends AppCompatActivity implements BottomSheetDialog
         });
     }
 
+    private void initRecyclerView()
+    {
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerViewAdapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(recyclerViewAdapter);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.divider_line, null));
+        recyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
     @Override
     public void onAddButtonClicked(String text)
     {
         list.add(text);
         recyclerViewAdapter.notifyDataSetChanged();
     }
+
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            list.remove(viewHolder.getAdapterPosition());
+            recyclerViewAdapter.notifyDataSetChanged();
+        }
+    };
 }
